1cf1dbf27a13b8a27ed8bd1f6b0dee7b362bc700
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-04-05 18:27:50 +0200

    3-4

--- query.sql
+++ query.sql
@@ -5,3 +5,11 @@ WHERE YEAR(`date_of_birth`) = 1990;
 SELECT *
 FROM `courses`
 WHERE `cfu` > 10;
+
+SELECT *
+FROM `students`
+WHERE 2022 - YEAR(`date_of_birth`) > 30;
+
+SELECT *
+FROM `courses`
+WHERE `period` = 'I semestre' AND `year` = 1;
